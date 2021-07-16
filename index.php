0977820bc65192537bb85a4c525d179588280ce1
dihugo <zuritahugo23@gmail.com>
2021-07-16 11:18:52 -0400

    index.php

---
+++ index.php
@@ -0,0 +1,34 @@
+<html>
+<title></title>
+<body>
+<h2>Servicio</h2>
+<div id="header">
+			<ul class="nav">
+				<li><a href="http://stock.com/">Servicio</a></li>
+				<li><a href=""></a>
+                    <ul>
+					 <li><a href="">Garantia</a></li>
+                        <ul>
+						  <li><a href="http://stock.com/registroProducto.php">Registro Garantia</a></li>
+                        </ul>
+                    </ul>
+                    <ul>
+					<li><a href="">Servicio Tecnico</a></li>
+							<ul>
+								<li><a href="http://stock.com/registroProducto.php">Registro Servicio Tecnico</a></li>
+							</ul>
+						</li>
+					</ul>
+                    <ul>
+					<li><a href="">Seguimiento</a></li>
+                    </ul>
+				</li>
+				<li><a href="">Acerca de</a>
+					<ul>
+					</ul>
+				</li>
+				<li><a href="">Contacto</a></li>
+			</ul>
+		</div>
+</body>
+</html>
